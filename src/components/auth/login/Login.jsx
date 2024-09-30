a041f390859ea0c79ebdcbbbad9fea00f4f79b3c
Vinh Son <sonpv07@gmail.com>
2024-09-30 17:58:55 +0700

    Add overlay

--- src/components/auth/login/Login.jsx
+++ src/components/auth/login/Login.jsx
@@ -5,13 +5,16 @@ import { Button, Divider, Form, Input } from "antd";
 import AuthService from "../../../services/auth.service";
 import { AuthContext } from "../../../context/AuthContext";
 import UserService from "../../../services/user.service";
+import { toast } from "react-toastify";
+import Overlay from "../../overlay/Overlay";
 
 export default function Login() {
   const [isLoading, setIsLoading] = useState(false);
 
   const [errorMessage, setErrorMessage] = useState("");
 
-  const { setUser, setAccessToken, setShowForm } = useContext(AuthContext);
+  const { setUser, setAccessToken, showForm, setShowForm } =
+    useContext(AuthContext);
 
   const handleLogin = async (values) => {
     console.log("Received values of form: ", values);
@@ -26,17 +29,19 @@ export default function Login() {
       if (response.success) {
         console.log(response.data);
 
-        const user = await UserService.getProfile();
+        const user = await UserService.getProfile(response.data.id);
 
         if (user) {
-          setUser(user);
-          localStorage.setItem("user", JSON.stringify(user));
+          setUser(user.data);
+          localStorage.setItem("user", JSON.stringify(user.data));
 
           setAccessToken(response.data.accessToken);
           localStorage.setItem(
             "token",
             JSON.stringify(response.data.accessToken)
           );
+
+          toast.success(response.message);
         }
       } else {
         setErrorMessage(response.message);
@@ -51,82 +56,88 @@ export default function Login() {
   console.log(errorMessage);
 
   return (
-    <div className="login-container">
-      <div className="title-container">
-        <p className="title">LOGIN</p>
-        <div className="close-btn">
-          <CloseOutlined onClick={() => setShowForm("")} />
-        </div>
-        <Divider className="divider" style={{ margin: 0 }} />
-      </div>
-
-      <div className="body-container">
-        <div className="mail-btn">
-          <img alt="google-icon" src="google.png" className="mail-img" />
-          <p>Continue with Google</p>
+    <Overlay isOpen={showForm === "LOGIN"} onClose={() => setShowForm("")}>
+      <div className="login-container">
+        <div className="title-container">
+          <p className="title">LOGIN</p>
+          <div className="close-btn">
+            <CloseOutlined onClick={() => setShowForm("")} />
+          </div>
+          <Divider className="divider" style={{ margin: 0 }} />
         </div>
 
-        <Divider className="divider" style={{ margin: 0 }}>
-          or
-        </Divider>
+        <div className="body-container">
+          <div className="mail-btn">
+            <img alt="google-icon" src="google.png" className="mail-img" />
+            <p>Continue with Google</p>
+          </div>
 
-        <div className="form-container">
-          <Form className="form" labelCol={{ span: 24 }} onFinish={handleLogin}>
-            <Form.Item
-              name="email"
-              rules={[
-                {
-                  required: true,
-                  message: "Please input your email",
-                },
-              ]}
+          <Divider className="divider" style={{ margin: 0 }}>
+            or
+          </Divider>
+
+          <div className="form-container">
+            <Form
+              className="form"
+              labelCol={{ span: 24 }}
+              onFinish={handleLogin}
             >
-              <Input
-                type="text"
+              <Form.Item
                 name="email"
-                placeholder="Email"
-                className="form-input"
-              />
-            </Form.Item>
-
-            <Form.Item
-              name="password"
-              rules={[
-                {
-                  required: true,
-                  message: "Please Input Password",
-                },
-              ]}
-            >
-              <Input
-                type="password"
-                name="password"
-                placeholder="Password"
-                className="form-input"
-              />
-            </Form.Item>
-
-            <div className="login-link">
-              <p>Don't have account?</p>
-              <p className="link" onClick={() => setShowForm("REGISTER")}>
-                Register now
-              </p>
-            </div>
-
-            {errorMessage && <p className="error-message">{errorMessage}</p>}
-            <Form.Item>
-              <Button
-                className="form-btn"
-                type="primary"
-                htmlType="submit"
-                loading={isLoading}
+                rules={[
+                  {
+                    required: true,
+                    message: "Please input your email",
+                  },
+                ]}
               >
-                Login
-              </Button>
-            </Form.Item>
-          </Form>
+                <Input
+                  type="text"
+                  name="email"
+                  placeholder="Email"
+                  className="form-input"
+                />
+              </Form.Item>
+
+              <Form.Item
+                name="password"
+                rules={[
+                  {
+                    required: true,
+                    message: "Please Input Password",
+                  },
+                ]}
+              >
+                <Input
+                  type="password"
+                  name="password"
+                  placeholder="Password"
+                  className="form-input"
+                />
+              </Form.Item>
+
+              <div className="login-link">
+                <p>Don't have account?</p>
+                <p className="link" onClick={() => setShowForm("REGISTER")}>
+                  Register now
+                </p>
+              </div>
+
+              {errorMessage && <p className="error-message">{errorMessage}</p>}
+              <Form.Item>
+                <Button
+                  className="form-btn"
+                  type="primary"
+                  htmlType="submit"
+                  loading={isLoading}
+                >
+                  Login
+                </Button>
+              </Form.Item>
+            </Form>
+          </div>
         </div>
       </div>
-    </div>
+    </Overlay>
   );
 }
--- src/components/auth/register/Register.jsx
+++ src/components/auth/register/Register.jsx
@@ -4,11 +4,13 @@ import { CloseOutlined } from "@ant-design/icons";
 import { Button, Divider, Form, Input } from "antd";
 import AuthService from "../../../services/auth.service";
 import { AuthContext } from "../../../context/AuthContext";
+import { toast } from "react-toastify";
+import Overlay from "../../overlay/Overlay";
 
 export default function Register() {
   const [isLoading, setIsLoading] = useState(false);
 
-  const { setShowForm } = useContext(AuthContext);
+  const { showForm, setShowForm } = useContext(AuthContext);
 
   const handleRegister = async (values) => {
     console.log("Received values of form: ", values);
@@ -19,8 +21,8 @@ export default function Register() {
       const response = await AuthService.register(values);
 
       if (response.success) {
-        setShowForm("LOGIN");
-        // Move to OTP Page
+        toast.success(response.message);
+        setShowForm("");
       }
     } catch (err) {
       console.log(err);
@@ -31,119 +33,121 @@ export default function Register() {
   };
 
   return (
-    <div className="login-container">
-      <div className="title-container">
-        <p className="title">SIGN UP</p>
-        <div className="close-btn">
-          <CloseOutlined onClick={() => setShowForm("")} />
-        </div>
-        <Divider className="divider" style={{ margin: 0 }} />
-      </div>
-
-      <div className="body-container">
-        <div className="mail-btn">
-          <img alt="google-icon" src="google.png" className="mail-img" />
-          <p>Continue with Google</p>
+    <Overlay isOpen={showForm === "REGISTER"} onClose={() => setShowForm("")}>
+      <div className="login-container">
+        <div className="title-container">
+          <p className="title">SIGN UP</p>
+          <div className="close-btn">
+            <CloseOutlined onClick={() => setShowForm("")} />
+          </div>
+          <Divider className="divider" style={{ margin: 0 }} />
         </div>
 
-        <Divider className="divider" style={{ margin: 0 }}>
-          or
-        </Divider>
+        <div className="body-container">
+          <div className="mail-btn">
+            <img alt="google-icon" src="google.png" className="mail-img" />
+            <p>Continue with Google</p>
+          </div>
 
-        <div className="form-container">
-          <Form
-            className="form"
-            labelCol={{ span: 24 }}
-            onFinish={handleRegister}
-          >
-            <Form.Item
-              name="name"
-              rules={[
-                {
-                  required: true,
-                  message: "Please input your name",
-                },
-              ]}
+          <Divider className="divider" style={{ margin: 0 }}>
+            or
+          </Divider>
+
+          <div className="form-container">
+            <Form
+              className="form"
+              labelCol={{ span: 24 }}
+              onFinish={handleRegister}
             >
-              <Input
-                type="text"
+              <Form.Item
                 name="name"
-                placeholder="Name"
-                className="form-input"
-              />
-            </Form.Item>
-
-            <Form.Item
-              name="email"
-              rules={[
-                {
-                  required: true,
-                  message: "Please input your email",
-                },
-              ]}
-            >
-              <Input
-                type="text"
-                name="email"
-                placeholder="Email"
-                className="form-input"
-              />
-            </Form.Item>
-
-            <Form.Item
-              name="password"
-              rules={[
-                {
-                  required: true,
-                  message: "Please Input Password",
-                },
-              ]}
-            >
-              <Input
-                type="password"
-                name="password"
-                placeholder="Password"
-                className="form-input"
-              />
-            </Form.Item>
-
-            <Form.Item
-              name="confirmPassWord"
-              rules={[
-                {
-                  required: true,
-                  message: "Please Input Password",
-                },
-              ]}
-            >
-              <Input
-                type="password"
-                name="confirmPassWord"
-                placeholder="Input Password Again"
-                className="form-input"
-              />
-            </Form.Item>
-
-            <div className="login-link">
-              <p>Already have account?</p>
-              <p className="link" onClick={() => setShowForm("LOGIN")}>
-                Login now
-              </p>
-            </div>
-
-            <Form.Item>
-              <Button
-                className="form-btn login"
-                type="primary"
-                htmlType="submit"
-                loading={isLoading}
+                rules={[
+                  {
+                    required: true,
+                    message: "Please input your name",
+                  },
+                ]}
               >
-                Sign Up
-              </Button>
-            </Form.Item>
-          </Form>
+                <Input
+                  type="text"
+                  name="name"
+                  placeholder="Name"
+                  className="form-input"
+                />
+              </Form.Item>
+
+              <Form.Item
+                name="email"
+                rules={[
+                  {
+                    required: true,
+                    message: "Please input your email",
+                  },
+                ]}
+              >
+                <Input
+                  type="text"
+                  name="email"
+                  placeholder="Email"
+                  className="form-input"
+                />
+              </Form.Item>
+
+              <Form.Item
+                name="password"
+                rules={[
+                  {
+                    required: true,
+                    message: "Please Input Password",
+                  },
+                ]}
+              >
+                <Input
+                  type="password"
+                  name="password"
+                  placeholder="Password"
+                  className="form-input"
+                />
+              </Form.Item>
+
+              <Form.Item
+                name="confirmPassWord"
+                rules={[
+                  {
+                    required: true,
+                    message: "Please Input Password",
+                  },
+                ]}
+              >
+                <Input
+                  type="password"
+                  name="confirmPassWord"
+                  placeholder="Input Password Again"
+                  className="form-input"
+                />
+              </Form.Item>
+
+              <div className="login-link">
+                <p>Already have account?</p>
+                <p className="link" onClick={() => setShowForm("LOGIN")}>
+                  Login now
+                </p>
+              </div>
+
+              <Form.Item>
+                <Button
+                  className="form-btn login"
+                  type="primary"
+                  htmlType="submit"
+                  loading={isLoading}
+                >
+                  Sign Up
+                </Button>
+              </Form.Item>
+            </Form>
+          </div>
         </div>
       </div>
-    </div>
+    </Overlay>
   );
 }
