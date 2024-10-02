1a10cf41df7fd3619d5b77ce95d5316e9f5dc471
Vinh Son <sonpv07@gmail.com>
2024-10-02 20:02:40 +0700

    Bind Data

--- src/pages/User/profile/view-profile/ViewProfile.jsx
+++ src/pages/User/profile/view-profile/ViewProfile.jsx
@@ -1,34 +1,30 @@
-import React, { useState, useEffect } from 'react';
-import { useNavigate } from 'react-router-dom';  
-import './ViewProfile.scss';
+import React, { useState, useEffect, useContext } from "react";
+import { useNavigate } from "react-router-dom";
+import "./ViewProfile.scss";
+import { AuthContext } from "../../../../context/AuthContext";
 
-function ViewProfile () {
-  const [userInfo, setUserInfo] = useState({
-    name: '',
-    email: '',
-    phone: '',
-    password: '', 
-    rating: 0,
-    ratingMax: 10,
-  });
-
+function ViewProfile() {
   const [showPassword, setShowPassword] = useState(false); // manage hide/show pass
 
-  const navigate = useNavigate();  // Initialize navigation
+  const { user } = useContext(AuthContext);
 
-  const fetchUserData = async () => {
-    try {
-      const response = await fetch('https://66f646f8436827ced976737d.mockapi.io/profile/1');
-      const data = await response.json();
-      setUserInfo(data);
-    } catch (error) {
-      console.error('Error fetching user data:', error);
-    }
-  };
+  const navigate = useNavigate(); // Initialize navigation
 
-  useEffect(() => {
-    fetchUserData();
-  }, []);
+  // const fetchUserData = async () => {
+  //   try {
+  //     const response = await fetch(
+  //       "https://66f646f8436827ced976737d.mockapi.io/profile/1"
+  //     );
+  //     const data = await response.json();
+  //     setUserInfo(data);
+  //   } catch (error) {
+  //     console.error("Error fetching user data:", error);
+  //   }
+  // };
+
+  // useEffect(() => {
+  //   fetchUserData();
+  // }, []);
 
   // Toggle hiển thị mật khẩu
   const toggleShowPassword = () => {
@@ -38,29 +34,40 @@ function ViewProfile () {
   return (
     <div className="container">
       <div className="profile">
-        <img
-          src="/avatar.jpg" 
-          alt="Avatar"
-          className="avatar"
-        />
+        <img src="/avatar.jpg" alt="Avatar" className="avatar" />
         <div className="info">
-          <h1>{userInfo.name}</h1>
-          <button className="edit-button" onClick={() => navigate('/edit-profile')}>Rename</button>
+          <h1>{user.name}</h1>
+          <button
+            className="edit-button"
+            onClick={() => navigate("/edit/name")}
+          >
+            Rename
+          </button>
           <h3>Profile</h3>
 
           <div className="field">
             <p>Email address:</p>
             <div className="field-edit">
-              <p>{userInfo.email}</p>
-              <button onClick={() => navigate('/edit-profile')} className="small-edit-button">Edit</button>
+              <p>{user.email}</p>
+              <button
+                onClick={() => navigate("/edit/email")}
+                className="small-edit-button"
+              >
+                Edit
+              </button>
             </div>
           </div>
 
           <div className="field">
             <p>Phone number:</p>
             <div className="field-edit">
-              <p>{userInfo.phone}</p>
-              <button onClick={() => navigate('/edit-profile')} className="small-edit-button">Edit</button>
+              <p>{user.contact}</p>
+              <button
+                onClick={() => navigate("/edit/contact")}
+                className="small-edit-button"
+              >
+                Edit
+              </button>
             </div>
           </div>
 
@@ -68,15 +75,27 @@ function ViewProfile () {
             <p>Password:</p>
             <div className="field-edit">
               {/* kt nếu showPassword là true thì hiển thị, ngược lại ẩn */}
-              <p>{showPassword ? userInfo.password : '*'.repeat(userInfo.password.length)}</p>
-              <button onClick={toggleShowPassword} className="small-edit-button">
-                {showPassword ? 'Hide' : 'Show'}
+              <p>
+                {showPassword
+                  ? user.password
+                  : "*".repeat(user.password.length)}
+              </p>
+              <button
+                onClick={toggleShowPassword}
+                className="small-edit-button"
+              >
+                {showPassword ? "Hide" : "Show"}
               </button>
-              <button onClick={() => navigate('/edit-profile')} className="small-edit-button">Change</button>
+              <button
+                onClick={() => navigate("/edit/password")}
+                className="small-edit-button"
+              >
+                Change
+              </button>
             </div>
           </div>
 
-          <p>Average Rating: {userInfo.rating}/{userInfo.ratingMax}</p>
+          <p>Average Rating: {user.average_feedback ?? 0}/5</p>
         </div>
       </div>
     </div>
