02679a288f7253e8d5c27666dd09c426161e932e
Vinh Son <sonpv07@gmail.com>
2024-10-10 19:05:16 +0700

    Update Layout

--- src/layout/Layout.jsx
+++ src/layout/Layout.jsx
@@ -2,12 +2,15 @@ import React from "react";
 import { Outlet } from "react-router-dom";
 import Header from "../components/header/Header";
 import Footer from "../components/footer/Footer";
+import "./Layout.scss";
 
 export default function Layout() {
   return (
-    <div>
+    <div className="layout">
       <Header />
-      <Outlet />
+      <div className="content">
+        <Outlet />
+      </div>
       <Footer />
     </div>
   );
