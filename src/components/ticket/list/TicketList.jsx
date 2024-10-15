88017820ce8a7561fd595f007a314b9355689076
Vinh Son <sonpv07@gmail.com>
2024-10-15 15:34:18 +0700

    Fix fetch API in Shopping

--- src/components/ticket/list/TicketList.jsx
+++ src/components/ticket/list/TicketList.jsx
@@ -3,34 +3,8 @@ import "./TicketList.scss";
 import TicketCard from "../card/TicketCard";
 import TicketService from "../../../services/ticket.service";
 
-export default function TicketList() {
-  const [ticketList, setTicketList] = useState([]);
-
+export default function TicketList({ ticketList, isLoading }) {
   // const [sellerInfo, setSellerInfo] = useState();
-
-  const [isLoading, setIsLoading] = useState(true);
-
-  const fetchApi = async () => {
-    try {
-      const response = await TicketService.getTicketList();
-
-      if (response.success) {
-        const filterData = response.data.filter(
-          (item) => item.status === "Available"
-        );
-
-        setTicketList(filterData);
-      }
-    } catch (error) {
-      console.error(error);
-    } finally {
-      setIsLoading(false);
-    }
-  };
-
-  useEffect(() => {
-    fetchApi();
-  }, []);
 
   return (
     <div className="ticket-list__grid">
--- src/pages/Ticket/Shopping/Shopping.jsx
+++ src/pages/Ticket/Shopping/Shopping.jsx
@@ -1,16 +1,17 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import "./Shopping.scss";
 import TicketList from "../../../components/ticket/list/TicketList";
 import { Select } from "antd";
+import TicketService from "../../../services/ticket.service";
 
 export default function Shopping() {
-  const [locations, setLocations] = useState([]);
+  const [location, setLocation] = useState(null);
 
-  const handleChange = (value) => {
-    setLocations(value);
-  };
+  const [category, setCategory] = useState(null);
 
-  console.log(locations);
+  const [ticketList, setTicketList] = useState([]);
+
+  const [isLoading, setIsLoading] = useState(true);
 
   const options = [
     { label: "Ha Noi", value: "Ha Noi" },
@@ -25,76 +26,37 @@ export default function Shopping() {
     { label: "Other", value: "Other" },
   ];
 
-  const ticketData = [
-    {
-      price: 50,
-      category: "Concert",
-      quantity: 100,
-      soldQuantity: 75,
-      isNegotiation: true,
-      name: "Black Pink Concert",
-      ticket_detail:
-        "General admission for the main event area. Standing room only.",
-      seller_info: {
-        name: "John Doe",
-        rating: 4.5,
-      },
-    },
-    {
-      price: 100,
-      category: "Sport",
-      quantity: 50,
-      soldQuantity: 20,
-      isNegotiation: true,
-      name: "Black Pink Concert",
+  const handleFilter = async () => {
+    const response = await TicketService.filterTicket(location, category);
 
-      ticket_detail:
-        "VIP access includes premium seating and a meet-and-greet with the performers.",
-      seller_info: {
-        name: "Jane Smith",
-        rating: 4.8,
-      },
-    },
-    {
-      price: 75,
-      category: "Festival",
-      quantity: 200,
-      soldQuantity: 180,
-      name: "Black Pink Concert",
-      ticket_detail:
-        "Limited early bird tickets at a discounted rate. Valid for general admission.",
-      seller_info: {
-        name: "Bob Johnson",
-        rating: 4.2,
-      },
-    },
-    {
-      price: 120,
-      category: "Festival",
-      quantity: 20,
-      soldQuantity: 5,
-      name: "Black Pink Concert",
-      ticket_detail:
-        "Exclusive backstage pass. Includes all VIP benefits plus a tour of the backstage area.",
-      seller_info: {
-        name: "Alice Brown",
-        rating: 4.9,
-      },
-    },
-    {
-      price: 120,
-      category: "Festival",
-      quantity: 20,
-      soldQuantity: 5,
-      name: "Black Pink Concert",
-      ticket_detail:
-        "Exclusive backstage pass. Includes all VIP benefits plus a tour of the backstage area.",
-      seller_info: {
-        name: "Alice Brown",
-        rating: 4.9,
-      },
-    },
-  ];
+    if (response.success) {
+      setTicketList(
+        response.data.filter((item) => item.status === "Available")
+      );
+    }
+  };
+
+  const fetchApi = async () => {
+    try {
+      const response = await TicketService.getTicketList();
+
+      if (response.success) {
+        const filterData = response.data.filter(
+          (item) => item.status === "Available"
+        );
+
+        setTicketList(filterData);
+      }
+    } catch (error) {
+      console.error(error);
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    fetchApi();
+  }, []);
 
   return (
     <div>
@@ -109,7 +71,7 @@ export default function Shopping() {
                 style={{ width: "100%", height: "50px" }}
                 className="custom-select"
                 placeholder="Select locations..."
-                onChange={handleChange}
+                onChange={(value) => setLocation(value)}
                 options={options}
               />
             </div>
@@ -120,31 +82,23 @@ export default function Shopping() {
                 style={{ width: "100%", height: "50px" }}
                 className="custom-select"
                 placeholder="Select ticket category..."
-                onChange={handleChange}
+                onChange={(value) => setCategory(value)}
                 options={categoryOptions}
               />
             </div>
             <div className="shopping__filter-option__item">
-              <button className="shopping__filter-option__item__button">
+              <button
+                className="shopping__filter-option__item__button"
+                onClick={handleFilter}
+              >
                 Filter
               </button>
             </div>
-            {/* <div className="shopping__filter-option__item">
-            <Select
-              mode="multiple"
-              allowClear
-              style={{ width: "100%" }}
-              className="custom-select"
-              placeholder="Select locations..."
-              onChange={handleChange}
-              options={options}
-            />
-          </div> */}
           </div>
 
           {/* Add filter options here */}
         </div>
-        <TicketList ticketData={ticketData} />
+        <TicketList isLoading={isLoading} ticketList={ticketList} />
       </div>
     </div>
   );
