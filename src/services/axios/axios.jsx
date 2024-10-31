b475ac0e8c85d8758d8fe89aaa7df6b84ea6bc77
Vinh Son <sonpv07@gmail.com>
2024-10-31 07:39:54 +0700

    Fix axios

--- src/services/axios/axios.jsx
+++ src/services/axios/axios.jsx
@@ -2,8 +2,12 @@ import axios from "axios";
 
 // API LOCAL
 // const baseUrl = "http://localhost:5212/api/";
+// const baseUrl = "https://localhost:7216/api/";
 
-const baseUrl = "http://14.225.204.144:7070/api/";
+// const baseUrl = "http://14.225.204.144:7070/api/";
+
+// HTTPS SERVER
+const baseUrl = "https://swp.vinhuser.one/api/";
 
 const config = {
   baseUrl: baseUrl,
@@ -14,30 +18,11 @@ const api = axios.create(config);
 api.defaults.baseURL = baseUrl;
 
 const handleBefore = (config) => {
-  const token = localStorage?.getItem("token")?.replaceAll('"', "");
+  const token = localStorage.getItem("token")?.replaceAll('"', "");
   config.headers["Authorization"] = `Bearer ${token}`;
   return config;
 };
 
 api.interceptors.request.use(handleBefore, null);
-
-export const getCustomerOrders = async (customerId) => {
-  try {
-    const response = await axios.get(`${baseUrl}/orders`, {
-      params: {
-        status: "success",
-        customerId: customerId,
-      },
-    });
-    console.log("Response from customer orders:", response);
-    return response.data;
-  } catch (error) {
-    console.error(
-      "Error fetching customer orders:",
-      error.response?.data || error.message
-    );
-    throw error;
-  }
-};
 
 export default api;
